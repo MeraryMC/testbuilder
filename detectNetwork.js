bff2a15ef0fc6002cdb224ff6f853e7949d63b96
Merary Martinez-Cobian <40481640+MeraryMC@users.noreply.github.com>
2018-09-27 22:35:09 -0700

    Added function to check 2 types of card networks
    *added function to check if the card number is a Diner's Club card or if it's an American Express card

--- detectNetwork.js
+++ detectNetwork.js
@@ -8,11 +8,19 @@
 //   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
+	if (cardNumber.length === 14 && (cardNumber.startsWith('38') || cardNumber.startsWith('39'))) {
+		return "Diner's Club";
+	} else if (cardNumber.length === 15 && (cardNumber.startsWith('34') || cardNumber.startsWith('37'))) {
+		return 'American Express';
+	} else {
+		return 'card unknown';
+	} 
+};
+
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
-};
 
 
